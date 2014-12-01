103f53aa6a0e55c0f9804fdec0fae22fe45b92e8
Anush <anush@techstream.org>
2014-12-02 01:05:17 +0530

    Initial Mockup Complete

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$("nav select").change(function() {
+	window.location = $(this).find("option:selected").val();
+})
+// Create the dropdown base
+$("<select />").appendTo("nav");
+$("nav select").addClass("show-for-small-only");
+
+// Create default option "Go to..."
+$("<option />", {
+	"selected": "selected",
+	"value"   : "",
+	"text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function() {
+	var el = $(this);
+	$("<option />", {
+		 "value"   : el.attr("href"),
+		 "text"    : el.text()
+	}).appendTo("nav select");
+});
